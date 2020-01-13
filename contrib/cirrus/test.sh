28790abe800de7fb11fbd4a61cf7a58f901f618c
Chris Evich <cevich@redhat.com>
2020-01-13 12:56:13 -0500

    Bors: Enable github app support
    Add initial support for Bors-ng replacement for Homu providing "ever
    green" merges of PRs.  Ref: https://bors.tech/
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- contrib/cirrus/test.sh
+++ contrib/cirrus/test.sh
@@ -10,7 +10,7 @@ if [[ "$IN_PODMAN" == "true" ]]
 then
     cd $GOSRC
     # Host build environment != container environment
-    make clean
+    showrun make clean
     in_podman --rm $IN_PODMAN_NAME:latest $0 $1
 elif [[ -z "$CROSS_TARGET" ]]
 then
@@ -21,10 +21,20 @@ then
 
     case $1 in
         validate)
+            showrun ooe.sh git remote add upstream "$CIRRUS_REPO_CLONE_URL"
+            showrun ooe.sh git remote update
+            if [[ -z "$CIRRUS_PR" ]]
+            then
+                echo "Testing a branch, assumed or based on the $DEST_BRANCH brach from .cirrus.yml"
+                export GITVALIDATE_EPOCH="$(git rev-parse upstream/$DEST_BRANCH)"
+            else  # Testing a PR
+                echo "Testing a PR targeted at the $DEST_BRANCH branch"
+                export GITVALIDATE_EPOCH="$CIRRUS_BASE_SHA" # relative to $CIRRUS_BASE_BRANCH
+            fi
+            export GITVALIDATE_TIP="$CIRRUS_CHANGE_IN_REPO"
+            echo "Linting & Validating from $GITVALIDATE_EPOCH to $GITVALIDATE_TIP"
             # Required for specifying our own commit range to git-validate.sh
             export TRAVIS=true
-            export GITVALIDATE_EPOCH="$CIRRUS_BASE_SHA"
-            export GITVALIDATE_TIP="$CIRRUS_CHANGE_IN_REPO"
             # The big 'Golint: can't lint 3 files...' warning puke, harmless and fixed in v1.20.0
             showrun make lint
             # TODO: This will fail if PR HEAD != upstream branch head
